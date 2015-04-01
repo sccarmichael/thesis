a5f5876032597ec051574c3e618e96099668783e
sccarmichael <sccarmichael@Sams-MacBook-Pro.local>
2015-04-01 15:03:02 -0400

    Added security and login

--- Likemind/thesis.js
+++ Likemind/thesis.js
@@ -1,65 +1,75 @@
-// simple-todos.js
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
-  
-// Replace the existing Template.body.helpers
+
+  Meteor.subscribe("tasks");
+
 Template.body.helpers({
-  tasks: function () {
-    if (Session.get("hideCompleted")) {
-      // If hide completed is checked, filter tasks
-      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: 1}});
-    } else {
-      // Otherwise, return all of the tasks
-      return Tasks.find({}, {sort: {createdAt: 1}});
-    }
-  },
-  hideCompleted: function () {
-    return Session.get("hideCompleted");
-  }
-});
+    tasks: function () {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+  });
 
-Template.body.events({
-  "submit .new-task": function (event) {
 
-    var text = event.target.text.value;
+  Template.body.events({
+    "submit .new-task": function (event) {
 
-    Tasks.insert({
-      text: text,
-      createdAt: new Date(),            // current time
-      owner: Meteor.userId(),           // _id of logged in user
-      username: Meteor.user().username  // username of logged in user
-    });
+      var text = event.target.text.value;
+
+      Meteor.call("addTask", text);
 
     // Clear form
     event.target.text.value = "";
 
     // Prevent default form submit
     return false;
-  },
-  // Add to Template.body.events
-  "change .hide-completed input": function (event) {
-    Session.set("hideCompleted", event.target.checked);
   }
+
 });
 
-Template.task.events({
-  "click .toggle-checked": function () {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {$set: {checked: ! this.checked}});
-  },
-  "click .delete": function () {
-    Tasks.remove(this._id);
-  }
-});
+  Template.task.events({
+    "click .delete": function () {
+      Meteor.call("deleteTask", this._id);
+    }
+  });
 
-Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
-});
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Template.task.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+
+});
+
+Meteor.publish("tasks", function () {
+  return Tasks.find({
+    $or: [
+    { owner: this.userId }
+    ]
+  });
+});
