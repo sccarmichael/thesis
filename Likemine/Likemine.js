65f9cbc42517edab5da779f7c1e550ab2005c39b
Samuel Carmichael <sccarmichael@Samuel-Carmichaels-iMac.local>
2015-04-12 20:34:25 -0400

    Fixed Sign-in

--- Likemine/Likemine.js
+++ Likemine/Likemine.js
@@ -4,11 +4,11 @@ if (Meteor.isClient) {
 
 	Meteor.subscribe("likes");
 
-  Template.body.helpers({
-    likes: function () {
-      return Likes.find({}, {sort: {createdAt: -1}});
-    }
-  });
+	Template.body.helpers({
+		likes: function () {
+			return Likes.find({}, {sort: {createdAt: -1}});
+		}
+	});
 
 	Template.body.helpers({
 		likes: function () {
@@ -23,17 +23,23 @@ if (Meteor.isClient) {
 
 			Meteor.call("addLike", text);
 
-    event.target.text.value = "";
-    return false;
-}
+			event.target.text.value = "";
+			return false;
+		}
 
-});
+	});
 
 	Template.like.events({
 		"click .delete": function () {
 			Meteor.call("deleteLike", this._id);
 		}
 	});
+
+	  Template.like.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
 
 	Accounts.ui.config({
 		passwordSignupFields: "USERNAME_ONLY"
@@ -42,30 +48,30 @@ if (Meteor.isClient) {
 
 Meteor.methods({
 	addLike: function (text) {
-    if (! Meteor.userId()) {
-    	throw new Meteor.Error("not-authorized");
-    }
+		if (! Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}
 
-    Likes.insert({
-    	text: text,
-    	createdAt: new Date(),
-    	owner: Meteor.userId(),
-    	username: Meteor.user().username
-    });
-},
+		Likes.insert({
+			text: text,
+			createdAt: new Date(),
+			owner: Meteor.userId(),
+			username: Meteor.user().username
+		});
+	},
 
-deleteLike: function (likeId) {
-	Likes.remove(likeId);
-},
+	deleteLike: function (likeId) {
+		Likes.remove(likeId);
+	},
 
 });
 
 if (Meteor.isServer) {
-  Meteor.publish("likes", function () {
-    return Likes.find({
-		$or: [
-		{ owner: this.userId }
-		]
+	Meteor.publish("likes", function () {
+		return Likes.find({
+			$or: [
+			{ owner: this.userId }
+			]
+		});
 	});
-});
 }
