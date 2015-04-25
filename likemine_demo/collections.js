89539ac1ab532b451a97607662cb26af84b1cd4d
sccarmichael <sccarmichael@Sams-MacBook-Pro.local>
2015-04-24 21:05:14 -0400

    Added Likemine_Demo

---
+++ likemine_demo/collections.js
@@ -0,0 +1,12 @@
+Likes = new Mongo.Collection("likes");
+
+
+if (Meteor.isServer) {
+
+  Accounts.removeOldGuests();
+
+Meteor.publish("likes", function () {
+  return Likes.find();
+});
+}
+
---
+++ likemine_demo/helpers.js
@@ -0,0 +1,44 @@
+if (Meteor.isClient) {
+
+    Meteor.subscribe("likes");
+
+  Template.input.helpers({
+    likes: function () {
+      return Likes.find({owner:Meteor.userId()}, {fields:{"text":1}},{sort: {createdAt: -1}});
+    }
+  });
+
+  Template.input.events({
+    "submit .new-like": function (event) {
+      var text = event.target.text.value;
+      Meteor.call("addLike", text);
+      event.target.text.value = "";
+      return false;
+    }
+
+  });
+
+  Template.like.events({
+    "click .delete": function () {
+      Meteor.call("deleteLike", this._id);
+    }
+  });
+
+  Template.like.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
+
+    Template.results.helpers({
+    recs: function () {
+      // Meteor.call("ignoreCurrentUser", Meteor.userId());
+      return Likes.find({owner:{$ne:Meteor.userId()}},{fields:{"text":1,"username":1}},{sort:{createdAt: -1}});
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+}
